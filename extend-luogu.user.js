e218c1a15b0e29d3ffb243fc5590e6b1dcd53aeb
optimize-2 <55306215+optimize-2@users.noreply.github.com>
2021-03-13 15:25:42 +0800

    2.2

--- extend-luogu.user.js
+++ extend-luogu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         extend-luogu
 // @namespace    http://tampermonkey.net/
-// @version      2.1.6
+// @version      2.2
 // @description  make the Luogu more powerful.
 // @author       optimize_2 ForkKILLET
 // @match        https://www.luogu.com.cn/*
@@ -13,7 +13,7 @@
 // @require      https://cdn.luogu.com.cn/js/jquery-2.1.1.min.js
 // ==/UserScript==
 
-const version = "2.1.6"
+const version = "2.2"
 
 function checkUpdate() {
     setTimeout(function() {
@@ -26,8 +26,9 @@ function checkUpdate() {
                 var wrap=document.createElement("div");
                 var first=document.body.firstChild;
                 var wraphtml=document.body.insertBefore(wrap,first);
-                wrap.innerHTML = `
-                <button type="button" class="am-btn am-btn-warning am-btn-block" onclick="window.open('/paste/fnln7ze9')">您的 extend-luogu 不是最新版本. 点我更新</button>`
+                wrap.innerHTML = `<button type="button" class="am-btn am-btn-warning am-btn-block" onclick="window.open('/paste/fnln7ze9')">您的 extend-luogu 不是最新版本. 点我更新</button>`
+
+                show_alert("extend-luogu", "您的 extend-luogu 不是最新版本. 请尽快更新")
             }
         });
     }, 1000)
@@ -300,6 +301,63 @@ const init = () => {
 
             })
         .appendTo($("ul#home-center-nav.am-nav.am-nav-pills.am-nav-justify"))
+
+        //random jump
+        //https://greasyfork.org/zh-CN/scripts/390181-%E6%B4%9B%E8%B0%B7%E6%8C%89%E9%9A%BE%E5%BA%A6%E9%9A%8F%E6%9C%BA%E8%B7%B3%E9%A2%98/code
+        $('document').ready(function(){setTimeout(function () {
+            $sidebar = $('#app-old .lg-index-content .lg-right.am-u-lg-3');
+            $firstele = $($sidebar.children()[0]);
+            // console.log($firstele);
+            $finder = $(`
+                <div class="lg-article" id="rand-problem-form">
+                    <h2>按难度随机跳题</h2>
+                    <select class="am-form-field" style="background-color:#DDD;" name="rand-problem-rating" autocomplete="off" placeholder="选择难度">
+                        <option value="0">暂无评定</option>
+                        <option value="1">入门</option>
+                        <option value="2">普及-</option>
+                        <option value="3">普及/提高-</option>
+                        <option value="4">普及+/提高</option>
+                        <option selected value="5">提高+/省选-</option>
+                        <option value="6">省选/NOI-</option>
+                        <option value="7">NOI/NOI+/CTSC</option>
+                    </select>
+                    <select class="am-form-field" style="background-color:#DDD;margin-top:16px;" name="rand-problem-source" autocomplete="off" placeholder="选择来源">
+                        <option selected value="P">洛谷题库</option>
+                        <option value="CF">CodeForces</option>
+                        <option value="SP">SPOJ</option>
+                        <option value="AT">AtCoder</option>
+                        <option value="UVA">UVa</option>
+                    </select>
+                    <button class="am-btn am-btn-sm am-btn-primary" style="margin-top:16px;visibility:hidden">跳转</button>
+                    <button class="am-btn am-btn-sm am-btn-primary lg-right" id="rand-problem-button" style="margin-top:16px;">跳转</button>
+                </div>
+            `);
+            $finder.insertAfter($firstele);
+            $('#rand-problem-button').click(function() {
+                $('#rand-problem-button').addClass('am-disabled');
+                $.get("https://www.luogu.com.cn/problem/list?difficulty=" + $('[name=rand-problem-rating]')[0].value + "&type=" + $('[name=rand-problem-source]')[0].value + "&page=1&_contentOnly=1",
+                    function (data) {
+                        var arr = data;
+                        if (arr['code'] != 200) {
+                            $('#rand-problem-button').removeClass('am-disabled');
+                            show_alert("好像哪里有点问题", arr["message"]);
+                        } else {
+                            var problem_count = arr['currentData']['problems']['count'];
+                            var page_count = Math.ceil(problem_count / 50);
+                            var rand_page = Math.floor(Math.random()*page_count) + 1;
+                            $.get("https://www.luogu.com.cn/problem/list?difficulty=" + $('[name=rand-problem-rating]')[0].value + "&type=" + $('[name=rand-problem-source]')[0].value + "&page=" + rand_page + "&_contentOnly=1",
+                                function(data) {
+                                    var list = data['currentData']['problems']['result'];
+                                    var rand_idx = Math.floor(Math.random()*list.length);
+                                    var pid = list[rand_idx]['pid'];
+                                    location.href = "https://www.luogu.com.cn/problem/" + pid;
+                                }
+                            );
+                        }
+                    }
+                );
+            });
+          },500)});
     }
 
     if (window.location.href === "https://www.luogu.com.cn/blog/311930/") {
