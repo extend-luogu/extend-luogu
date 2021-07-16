8d38a5bec99f6cdbe1613cf216ab197891e51243
汪心禾 Wang, Xinhe <wangxinhe06@gmail.com>
2021-07-16 12:54:22 +0800

    SUM [CI] Python linter workflows
    -@ Python linter workflows
       -@ Pylint workflow
        : Check `.py` files with Python linter `flake8` and `pylint`
       -@ Add black workflow
        : Check Python files with psf/black
    
    *# <.gitignore>
       -- Node.js ignores
        & https://github.com/github/gitignore/blob/master/Node.gitignore
       -- Python ignores
        & https://github.com/github/gitignore/blob/master/Python.gitignore
    
    *< <dashboard/.bundle.py>
     : Reformatted with psf/black

--- dashboard/.bundle.py
+++ dashboard/.bundle.py
@@ -1,31 +1,34 @@
-#!/bin/env python
+#!/bin/env python3
 
 import re
 
-html = open('index.html', 'r')
 
-text = html.readlines()
+with open("index.html", "r") as html:
+    text = html.readlines()
+
 for i, ln in enumerate(text):
     m = re.match(r'^\t<script src="(.*?)"></script>', ln)
     if m:
         fn = m.group(1)
-        js = open(fn, 'r').readlines()
-        text[i] = '\t<script>\n' + ''.join(js) + '\t</script>\n\n'
-        print('Bundling script <%s> @ %d' % (fn, i))
+        with open(fn, "r") as js:
+            text[i] = (
+                "\t<script>\n" + "".join(js.readlines()) + "\t</script>\n\n"
+            )
+        print("Bundling script <%s> @ %d" % (fn, i))
         continue
-    
-    m = re.match(r'^\t<link rel="stylesheet" type="text/css" href="(.*?)" />', ln)
+
+    m = re.match(
+        r'^\t<link rel="stylesheet" type="text/css" href="(.*?)" />', ln
+    )
     if m:
         fn = m.group(1)
-        css = open(fn, 'r').readlines()
-        text[i] = '\t<style>\n' + ''.join(css) + '\t</style>\n\n'
-        print('Bundling stylesheet <%s> @ %d' % (fn, i))
+        with open(fn, "r") as css:
+            text[i] = (
+                "\t<style>\n" + "".join(css.readlines()) + "\t</style>\n\n"
+            )
+        print("Bundling stylesheet <%s> @ %d" % (fn, i))
         continue
 
-html.close()
-
-html = open('bundle.html', 'w')
-
-html.writelines(text)
-print('Finishing bundling')
-
+with open("bundle.html", "w") as html:
+    html.writelines(text)
+print("Finishing bundling")
