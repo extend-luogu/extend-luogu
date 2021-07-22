77894ab82d4fee3bc0270243011d631bd5cf7628
博瀚君 <41739624+bohanjun@users.noreply.github.com>
2021-07-22 18:54:37 +0800

    VER 2.7.8 Judge Problem
    *M judge_problem
       $ judge_problem
        : Using more elegant way.

--- extend-luogu.user.js
+++ extend-luogu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           extend-luogu
 // @namespace      http://tampermonkey.net/
-// @version        2.7.7
+// @version        2.7.8
 //
 // @match          https://*.luogu.com.cn/*
 // @match          https://*.luogu.org/*
@@ -112,16 +112,15 @@ const springboard = (param, styl) => {
     return $sb
 }
 
-const judge_problem = (text) => { // Note: 判断字符串是否为题号, B不算在内
-    if (text.match(/^AT[1-9][0-9]{0,}$/i)) return true
-    if (text.match(/^CF[1-9][0-9]{0,}[A-Z][0-9]?$/i)) return true
-    if (text.match(/^SP[1-9][0-9]{0,}$/i)) return true
-    if (text.match(/^P[1-9][0-9]{3,}$/i)) return true
-    if (text.match(/^UVA[1-9][0-9]{2,}$/i)) return true
-    if (text.match(/^U[1-9][0-9]{0,}$/i)) return true
-    if (text.match(/^T[[1-9][0-9]{0,}$/i)) return true
-    return false
-}
+const judge_problem = text => [
+    /^AT[1-9][0-9]{0,}$/i,
+    /^CF[1-9][0-9]{0,}[A-Z][0-9]?$/i,
+    /^SP[1-9][0-9]{0,}$/i,
+    /^P[1-9][0-9]{3,}$/i,
+    /^UVA[1-9][0-9]{2,}$/i,
+    /^U[1-9][0-9]{0,}$/i,
+    /^T[[1-9][0-9]{0,}$/i
+].some(re => re.test(text))
 
 // ==/Utilities==
 
@@ -335,6 +334,7 @@ mod.reg_hook("dash-bridge", "控制桥", "@/.*", {
         color: white;
         border-radius: 6px;
         box-shadow: 0 0 7px dodgerblue;
+        cursor: pointer;
     }
     #exlg-dash > .exlg-warn {
         position: absolute;
