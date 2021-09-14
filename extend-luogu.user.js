d3b5ab950d2e41acd38418c20d9f87de8b5cc850
Minstdfx <56465435+Minstdfx@users.noreply.github.com>
2021-09-14 21:16:36 +0800

    VER 2.11.7
    *M dash-board
       *- data
        : a water commit that fix a little bug

--- extend-luogu.user.js
+++ extend-luogu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           extend-luogu
 // @namespace      http://tampermonkey.net/
-// @version        2.11.6
+// @version        2.11.7
 //
 // @match          https://*.luogu.com.cn/*
 // @match          https://*.luogu.org/*
@@ -495,7 +495,7 @@ mod.reg_main("dash-board", "控制面板", mod.path_dash_board, {
     },
     lang: {
         ty: "enum", dft: "en", vals: [ "en", "zh" ],
-        desc: [ "Language of descriptions in the dashboard", "控制面板提示语言" ]
+        info: [ "Language of descriptions in the dashboard", "控制面板提示语言" ]
     }
 }, () => {
     const novogui_modules = [
@@ -1777,7 +1777,6 @@ mod.reg_chore("sponsor-list", "获取标签列表", "1D", "@/.*", {
 mod.reg_hook_new("sponsor-tag", "标签显示", [ "@/", "@/paste", "@/discuss/.*", "@/problem/.*", "@/ranking.*" ], {
     tag_list: { ty: "string", priv: true }
 }, ({ args }) => {
-    const isDiscuss = /\/discuss\/.*/.test(location.href)
     // $("span.wrapper:has(a[target='_blank'][href]) > span:has(a[target='_blank'][href]):not(.hover):not(.exlg-sponsor-tag)").addClass("exlg-sponsor-tag") // Note: usernav的span大钩钩
     const tag_list = JSON.parse(sto["^sponsor-list"].tag_list)
     const add_badge = ($e) => {
@@ -1979,4 +1978,3 @@ $(() => {
     log("Launching")
     mod.execute()
 })
-
