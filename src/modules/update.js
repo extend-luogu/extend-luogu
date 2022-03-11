209e040aca649fb5dc7d98d6d285f3cb4c51c1d2
5ab-juruo <xiaowucc2017@163.com>
2022-03-11 23:43:48 +0800

    Make update logs human-readable
    -M update-log
     : 增加了对 update log 的翻译
     : 可以调整是否查看开发者相关的 log
    ^@ 对 "`" 进行了转义

--- src/modules/update.js
+++ src/modules/update.js
@@ -22,15 +22,60 @@ mod.reg_chore("update", "检查更新", "1D", ".*", null, () => {
     })
 })
 
+const dev_op = ["$", "#<@"].map(s => s.split(""))
+const human_lang = {
+    Ty: {
+        "-": "添加",
+        "x": "删减",
+        "!": "重大",
+        "*": "修改",
+        "^": "修复",
+        "$": "重构",
+    },
+    Op: {
+        "-": "特性",
+        "?": "文档",
+        "#": "依赖",
+        "<": "代码风格",
+        ">": "命令",
+        "M": "模块",
+        "H": "钩子",
+        "@": "GitHub Action",
+    }
+}
 mod.reg("update-log", "更新日志显示", "@/.*", {
     last_version: { ty: "string", priv: true },
+    style: { ty: "enum", vals: ["Commit Message", "自然语言"], get: "id", info: [
+        "The way to display log", "显示 Log 的方式"
+    ]},
+    keep_dev: { ty: "boolean", dft: true, info: [
+        "Keep developer messages", "保留开发者更新信息"
+    ]},
 }, ({ msto }) => {
     if (location.href.includes("blog")) return // Note: 如果是博客就退出
     const version = GM_info.script.version
+    let lstdo = false
     const fix_html = (str) => {
         let res = `<div class="exlg-update-log-text" style="font-family: ${sto["code-block-ex"].copy_code_font};">`
         str.split("\n").forEach(e => {
-            res += `<div>${e.replaceAll(" ", "&nbsp;")}</div>`
+            let trmde = e.trimStart()
+            if (!msto.keep_dev) {
+                if (trmde[0] === ":") {
+                    if (!lstdo)
+                        return
+                }
+                else if (dev_op.some((v, i) => v.some(c => (trmde[i] === c)))) {
+                    lstdo = false
+                    return
+                }
+                else
+                    lstdo = true
+            }
+            else
+                lstdo = false
+            if (msto.style === 1 && trmde[0] !== ":")
+                e = " ".repeat(e.length - trmde.length) + human_lang.Ty[trmde[0]] + human_lang.Op[trmde[1]] + trmde.substring(2)
+            res += `<div>${e.replace(/ /g, "&nbsp;")}</div>` // Note: 为了兼容不用 replaceAll
         })
         return res + "</div>"
     }
