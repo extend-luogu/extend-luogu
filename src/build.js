d765f8424dc956be01aa278ff629843b971c9b57
5ab-juruo <xiaowucc2017@163.com>
2022-03-19 21:45:17 +0800

    fix builder: fill ignored file[s] if it doesn't exist

--- src/build.js
+++ src/build.js
@@ -1,18 +1,28 @@
 /* eslint-env node */
 
 process.chdir("src")
+
+const
+    { existsSync, readFileSync, writeFileSync } = require("fs"),
+    { join, basename } = require("path")
+
+;(([fn, val]) => {
+    if (!existsSync(fn))
+        writeFileSync(fn, val)
+})(["./resources/update-log.js", "export default ``"])
+
 const parg = require("minimist")(process.argv.slice(2)),
     minify = parg.m ?? !parg.b,
-    outfn = require("path").join("..", parg.d ?? "dist", parg.o ?? "extend-luogu.user.js")
+    outfn = join("..", parg.d ?? "dist", parg.o ?? "extend-luogu.user.js")
 require("esbuild").buildSync({
     entryPoints: [ "main.js" ],
     outfile: outfn,
     banner: {
-        js: require("fs").readFileSync("resources/tm-headers.js", "utf8").replace("CUR_VER", process.env.npm_package_version) + "\n;",
+        js: readFileSync("resources/tm-headers.js", "utf8").replace("CUR_VER", process.env.npm_package_version) + "\n;",
     },
     bundle: true,
     charset: "utf8",
     minify
 })
-console.log(`Build ${require("path").basename(outfn)} successfully.`)
+console.log(`Build ${basename(outfn)} successfully.`)
 process.exit()
