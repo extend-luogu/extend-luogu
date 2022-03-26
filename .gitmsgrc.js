8b22ccafea161409402455b1f0adb72c6e873353
5ab <xiaowucc2017@163.com>
2022-03-26 10:26:43 +0800

    fix message checker

--- .gitmsgrc.js
+++ .gitmsgrc.js
@@ -1,11 +1,6 @@
 module.exports = {
 	debug: false,
 	itemTy: o => o.concat("MHDU".split("")),
-	checker: {
-		itemTy: N => {
-		if (N.l === 3 && N.c === "-")
-			throw `is root item, so shouldn't be "-" type`
-		}
-	}
+	checker: {}
 }
 
