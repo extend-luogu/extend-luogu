7e914354f2715e3dfa6f84ff974d71b67ae39c46
汪心禾 <wangxinhe06@gmail.com>
2022-04-06 20:59:51 +0800

    Make `cs_post` `async` and add `type` parameter

--- src/modules/captcha.js
+++ src/modules/captcha.js
@@ -3,20 +3,17 @@ import { $, cs_post } from "../utils.js"
 
 mod.reg("captcha", "验证码自动填充", [ "@/auth/login", "@/discuss/.+", "@/image" ], null, () => {
     let img = $("img[data-v-3e1b4641],#verify_img")
-    const autofill = () => {
+    const autofill = async () => {
         const canvas = document.createElement("canvas")
         canvas.width = img[0].width
         canvas.height = img[0].height
         canvas.getContext("2d").drawImage(img[0], 0, 0)
-        cs_post({
+        const input = $("input[placeholder$='验证码']")[0]
+        input.value = (await cs_post({
             url: "https://luogu-captcha-bypass.piterator.com/predict/",
             data: canvas.toDataURL("image/jpeg"),
-            onload: (res) => {
-                const input = $("input[placeholder$='验证码']")[0]
-                input.value = res.responseText
-                input.dispatchEvent(new Event("input"))
-            }
-        })
+        })).responseText
+        input.dispatchEvent(new Event("input"))
     }
     if (img.length) {  // /auth/login and /discuss/*
         img.click()
--- src/utils.js
+++ src/utils.js
@@ -122,13 +122,14 @@ const cs_get2 = url => new Promise((resolve, reject) => GM_xmlhttpRequest({
     onerror: e => reject(e)
 }))
 
-const cs_post = ({url, data, onload, onerror = err => error(err)}) => GM_xmlhttpRequest({
+const cs_post = ({url, data, type}) => new Promise((resolve, reject) => GM_xmlhttpRequest({
     url: url,
     method: "POST",
     data: data,
-    onload: onload,
-    onerror: onerror
-})
+    headers: { "Content-Type": type },
+    onload: e => resolve(e),
+    onerror: e => reject(e)
+}))
 
 const get_latest = callbackfn => {
     cs_get({
