03f2ddf21096f6ac84c50d54e6e2ac993e2cf874
汪心禾 <wangxinhe06@gmail.com>
2022-04-07 12:22:57 +0800

    Fix display issue in mod board
    ^M board
     : Fix the bug that Benben count does not show on the same line as its username

--- src/modules/board.js
+++ src/modules/board.js
@@ -30,8 +30,8 @@ mod.reg_board("benben-ranklist", "犇犇龙王排行榜", {
     show: { ty: "boolean", dft: true, info: [ "Show in default", "是否默认展开" ] }
 }, ({ msto, $board }) => {
     // Note: Add the title.
-    $board.html(`<h3 id="bb-rnklst-h2">犇犇排行榜 <span id="bb-rnklst-btn" class="bb-rnklst-span"> [<a>${ msto.show ? "收起" : "展开" }</a>]</span><span style="float: right;" class="bb-rnklst-span"> [<a id="refresh-bbrnk">刷新</a>]</span></h3><div style="display: ${ msto.show ? "block" : "none" }" id="bb-rnklst-div"></div>`)
-    const $list = $board.find("#bb-rnklst-div"), $fbtn = $board.find("#bb-rnklst-btn > a").on("click", () => {
+    $board.html(`<h3 id="bb-rnklst-h2">犇犇排行榜 <span id="bb-rnklst-btn" class="bb-rnklst-span"> [<a>${ msto.show ? "收起" : "展开" }</a>]</span><span style="float: right;" class="bb-rnklst-span"> [<a id="refresh-bbrnk">刷新</a>]</span></h3><ol style="display: ${ msto.show ? "block" : "none" }" id="bb-rnklst"></ol>`)
+    const $list = $board.find("#bb-rnklst"), $fbtn = $board.find("#bb-rnklst-btn > a").on("click", () => {
         msto.show = !msto.show
         $fbtn.text(msto.show ? "收起" : "展开")
         $list.toggle()
@@ -41,11 +41,12 @@ mod.reg_board("benben-ranklist", "犇犇龙王排行榜", {
         onload: function(res) {
             // let s=`<h3 id="bb-rnklst-h2">犇犇排行榜 <span id="bb-rnklst-btn" class="bb-rnklst-span"> [<a>${ msto.show ? "收起" : "展开" }</a>]</span><span style="float: right;" class="bb-rnklst-span"> [<a id="refresh-bbrnk">刷新</a>]</span></h3>`
             $(JSON.parse(res.response)).each((index, obj) => {
-                $(`<div class="bb-rnklst-${index + 1}">
-                    <span class="bb-rnklst-ind${(index < 9) ? (" bb-top-ten") : ("")}">${index + 1}.</span>
-                    <a href="https://bens.rotriw.com/user/${obj[2]}">${obj[1]}</a>
-                    <span style="float: right;">共 ${obj[0]} 条</span>
-                </div>`).appendTo($list)
+                $(`<li class="bb-rnkitm">
+                    <span>
+                        <a href="https://bens.rotriw.com/user/${obj[2]}">${obj[1]}</a>
+                        <span>${obj[0]} 条</span>
+                    </span>
+                </li>`).appendTo($list)
             })
         }
     })
