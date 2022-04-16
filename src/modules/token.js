03cb96e72dc6f49c51585afd8f22e71b358bf61d
汪心禾 <wangxinhe06@gmail.com>
2022-04-17 00:37:28 +0800

    Update token.js
    ^M token
     : 修复了无法获取token的问题
     : 缩短了获取token的间隔时间

--- src/modules/token.js
+++ src/modules/token.js
@@ -1,7 +1,7 @@
 import mod from "../core.js";
 import { cs_get2, cs_post, $ } from "../utils.js";
 
-mod.reg_chore("token", "exlg 令牌", "1D", "@/.*", {
+mod.reg_chore("token", "exlg 令牌", "10m", "@/.*", {
     token: { ty: "string", priv: true },
 }, async ({ msto }) => {
     if (unsafeWindow._feInjection.currentUser) {
@@ -18,20 +18,20 @@ mod.reg_chore("token", "exlg 令牌", "1D", "@/.*", {
                 ttl.status !== 401 && ttl.data >= 60 * 60 * 25
             ) return false;
         }
-        const paste_id = await cs_post(
+        const paste_id = (await cs_post(
             "https://www.luogu.com.cn/paste/new?_contentOnly",
             {
-                data: await cs_get2("https://exlg.piterator.com/token/generate").data,
+                data: (await cs_get2("https://exlg.piterator.com/token/generate")).data.data,
                 public: true,
             },
             {
                 "x-csrf-token": $("[name='csrf-token']").attr("content"),
                 referer: "https://www.luogu.com.cn/paste",
             },
-        ).data.id;
-        msto.token = await cs_get2(
+        )).data.id;
+        msto.token = (await cs_get2(
             `https://exlg.piterator.com/token/verify/${paste_id}`,
-        ).data.data.token;
+        )).data.data.token;
         await cs_post(
             `https://www.luogu.com.cn/paste/delete/${paste_id}?_contentOnly`,
             {},
