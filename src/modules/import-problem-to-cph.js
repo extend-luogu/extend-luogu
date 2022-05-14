4161387ecd5ba25216b25f2ab9ffb5b1c1112d1b
Lotuses-robot <87472564+Lotuses-robot@users.noreply.github.com>
2022-05-14 22:06:58 +0800

    Add import-problem-to-cps module -M import-problem-to-cps module  : 可以向 cph 传输题目数据

---
+++ src/modules/import-problem-to-cph.js
@@ -0,0 +1,82 @@
+import { $ } from "../utils.js";
+import mod from "../core.js";
+import exlg_alert from "../components/exlg-dialog-board.js";
+
+mod.reg_hook_new("import-problem-to-cph", "添加到 cph", ["@/problem/[A-Z]+[0-9]+(#.*)?", "@/record/.*"], null, ({ args }) => {
+    if (!args.length || window.location.href.search("/record/") !== -1) return;
+
+    function regi_exlg_cph() {
+        $("button.exlg-cph").click(() => {
+            $.get(window.location.href, (elem) => {
+                function htmlToElement(html) {
+                    return new DOMParser().parseFromString(html, "text/html").documentElement;
+                }
+
+                let p;
+                elem = htmlToElement(elem);
+                console.log(elem);
+                for (const scriptElem of elem.querySelectorAll("script")) {
+                    const script = scriptElem.textContent;
+                    if (script.startsWith("window._feInjection")) {
+                        const startQuoteIndex = script.indexOf("\"");
+                        const endQuoteIndex = script.substr(startQuoteIndex + 1).indexOf("\"");
+                        const encodedData = script.substr(startQuoteIndex + 1, endQuoteIndex);
+
+                        p = JSON.parse(decodeURIComponent(encodedData)).currentData.problem;
+                    }
+                }
+                if (p == null) {
+                    throw new Error("Failed to find problem data");
+                }
+                const dt = [];
+                for (const x of p.samples) {
+                    dt.push({ input: x[0], output: x[1] });
+                }
+
+                GM_xmlhttpRequest({
+                    url: "http://localhost:27121/",
+                    method: "POST",
+                    data: JSON.stringify({
+                        batch: { id: "exlgnb", size: 1 },
+                        name: p.pid,
+                        group: "Luogu",
+                        url: window.location.href,
+                        interactive: "false",
+                        memoryLimit: Math.floor(Math.max(...p.limits.memory) / 1024),
+                        timeLimit: Math.max(...p.limits.time),
+                        tests: dt,
+                        input: { type: "stdin" },
+                        output: { type: "stdout" },
+                        language: { java: { mainClass: "Main", taskClass: p.pid } },
+                        testType: "single",
+                    }),
+                    onload(res) {
+                        if (res.status === 502) {
+                            exlg_alert("未启动 cph，传输失败！");
+                        }
+                    },
+                    onerror() {
+                        exlg_alert("未启动 cph，传输失败！");
+                    },
+                });
+            });
+        });
+    }
+
+    if (window.location.href.search("#submit") === -1) {
+        $("button.lfe-form-sz-middle").addClass("lg-btm");
+        args.append("<button data-v-7ade990c=\"\" data-v-43063e73=\"\" type=\"button\" class=\"exlg-cph lfe-form-sz-middle\" data-v-2dfcfd35=\"\" style=\"border-color: rgb(52, 152, 219); background-color: rgb(52, 152, 219);\"> 传送至 cph </button>");
+        regi_exlg_cph();
+    }
+    $("button.lg-btm").click(() => {
+        if (window.location.href.search("#submit") === -1) {
+            args.append("<button data-v-7ade990c=\"\" data-v-43063e73=\"\" type=\"button\" class=\"exlg-cph lfe-form-sz-middle\" data-v-2dfcfd35=\"\" style=\"border-color: rgb(52, 152, 219); background-color: rgb(52, 152, 219);\"> 传送至 cph </button>");
+            regi_exlg_cph();
+        } else {
+            $("button.exlg-cph").remove();
+        }
+    });
+}, (e) => {
+    const $tmp = $(e.target).find("div.operation");
+    return { result: $tmp.length > 0, args: $tmp };
+}, () => $("div.operation"), "module");
