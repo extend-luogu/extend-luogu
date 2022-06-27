33079821ee154383a2c4b27ab77bf5cee9d4f77d
YuzhenQin <1@jin-dan.site>
2022-06-27 20:18:07 +0800

    Make milkdown work properly

--- src/modules/milkdown.js
+++ src/modules/milkdown.js
@@ -1,31 +1,51 @@
+import { Editor, rootCtx } from "@milkdown/core";
+import { nord } from "@milkdown/theme-nord";
+import { commonmark } from "@milkdown/preset-commonmark";
+import { listener, listenerCtx } from "@milkdown/plugin-listener";
+import uindow, { $ } from "../utils.js";
 import mod from "../core.js";
-import { $ } from "../utils.js";
-import { Editor } from '@milkdown/core';
-import { nord } from '@milkdown/theme-nord';
-import { commonmark } from '@milkdown/preset-commonmark';
 
 mod.reg_v2({
     name: "milkdown",
     info: "Milkdown",
     path: ["@/.*"],
-    cate: "module"
+    cate: "module",
 }, {}, (handler) => {
     handler.hook({
         name: "markdown-replace",
-        info: "替换 markdown 编辑器"
+        info: "替换 markdown 编辑器",
     }, null, ({ result, target }) => {
         if (!result) return;
-        target.each((_, e, $e=$(e), $p=$e.parent()) => {
-            $e.remove();
-            $(`<div class="exlg-milkdown"></div>`)
+        target.each((_, e, $e = $(e), $p = $e.parent()) => {
+            $e
+                .removeClass("exlg-milkdown-before")
+                .addClass("exlg-milkdown-mp")
+                .css("display", "none");
+            $(`<div data-v-6d5597b1 class="exlg-milkdown mp-editor-container"></div>`)
                 .appendTo($p);
-        })
+            Editor.make().config((ctx) => {
+                ctx.set(rootCtx, document.querySelector(".exlg-milkdown"));
+            }).config((ctx) => {
+                ctx.get(listenerCtx).markdownUpdated((_ctx, markdown, _prevMarkdown) => {
+                    uindow.markdownPalettes.content = markdown;
+                });
+            }).use(nord)
+                .use(commonmark)
+                .use(listener)
+                .create();
+        });
     }, () => {
-        const tmp = $(".mp-editor-container");
-        if (tmp.length > 0) {
+        const $tmp = $(".mp-editor-container[data-v-6d5597b1][data-v-aa62436e]");
+        $tmp.each((_, e, $e = $(e)) => {
+            if (!($e.hasClass("exlg-milkdown-before") || $e.hasClass("exlg-milkdown-mp"))) {
+                $e.addClass("exlg-milkdown-before");
+            }
+        });
+        const $target = $(".exlg-milkdown-before");
+        if ($target.length) {
             return {
                 result: true,
-                target: tmp
+                target: $target,
             };
         }
         return { result: false };
