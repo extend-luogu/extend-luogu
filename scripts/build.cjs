b366d0c89b939d2206e637e02850b3df59a663a5
ForkΨKILLET <fork_killet@qq.com>
2022-07-01 17:25:08 +0800

    style(build): fix typo and remove unnecessary try blocks

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,64 +1,56 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 const fs = require('node:fs/promises')
 const esbuild = require('esbuild')
 const vuePlugin = require('esbuild-plugin-vue3')
 const pkg = require('../package.json')
 
-/* eslint-disable no-console */
-
-const fail = (err) => {
-    console.error(`Failed %o`, err)
-    process.exit(1)
-}
-
-;(async () => {
+exports = async () => {
     const startTime = Date.now()
 
     console.log('Building core...')
-    await esbuild
-        .build({
-            entryPoints: ['src/core/index.ts'],
-            format: 'iife',
-            bundle: true,
-            minify: true,
-            outfile: 'dist/core.bundle.js'
-        })
-        .catch(fail)
+    await esbuild.build({
+        entryPoints: ['src/core/index.ts'],
+        format: 'iife',
+        bundle: true,
+        minify: true,
+        outfile: 'dist/core.bundle.js'
+    })
 
     console.log('Building dash...')
-    await esbuild
-        .build({
-            entryPoints: ['src/dash/src/main.ts'],
-            plugins: [vuePlugin()],
-            format: 'iife',
-            bundle: true,
-            minify: true,
-            outfile: 'dist/dash.bundle.js'
-        })
-        .catch(fail)
+    await esbuild.build({
+        entryPoints: ['src/dash/src/main.ts'],
+        plugins: [vuePlugin()],
+        format: 'iife',
+        bundle: true,
+        minify: true,
+        outfile: 'dist/dash.bundle.js'
+    })
 
     console.log('Concating... (header + dash + core)')
-    try {
-        const header = await fs.readFile('./resources/header.js', 'utf-8')
-        const coreJs = await fs.readFile('./dist/core.bundle.js', 'utf-8')
-        const dashJs = await fs.readFile('./dist/dash.bundle.js', 'utf-8')
-        const dashCss = await fs.readFile('./dist/dash.bundle.css', 'utf-8')
+    const header = await fs.readFile('./resources/header.js', 'utf-8')
+    const coreJs = await fs.readFile('./dist/core.bundle.js', 'utf-8')
+    const dashJs = await fs.readFile('./dist/dash.bundle.js', 'utf-8')
+    const dashCss = await fs.readFile('./dist/dash.bundle.css', 'utf-8')
 
-        const injectResource = (name, str) =>
-            `\n;unsafeWindow.exlgResources.${name} = ${JSON.stringify(str)};\n`
+    const injectResource = (name, str) =>
+        `\n;unsafeWindow.exlgResources.${name} = ${JSON.stringify(str)};\n`
 
-        const concated = `${header.replace(
-            '{{version}}',
-            pkg.version
-        )}\n;unsafeWindow.exlgResources = {};\n${injectResource(
-            'dashJs',
-            dashJs
-        )}${injectResource('dashCss', dashCss)}\n${coreJs}`
-        await fs.writeFile('./dist/core.bundle.js', concated)
-    } catch (err) {
-        fail(err)
-    }
+    const concated =
+        // eslint-disable-next-line prefer-template
+        header.replace('{{version}}', pkg.version) +
+        `\n;unsafeWindow.exlgResources = {};\n` +
+        injectResource('dashJs', dashJs) +
+        injectResource('dashCss', dashCss) +
+        coreJs
+    await fs.writeFile('./dist/core.bundle.js', concated)
 
-    console.log('Exlg builded in %d ms.', Date.now() - startTime)
-})()
+    console.log('Built exlg in %d ms.', Date.now() - startTime)
+}
+
+try {
+    exports()
+} catch (err) {
+    console.error('Failed to build exlg: %o', err)
+}
