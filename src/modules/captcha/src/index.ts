51052ae4c9aa8ea9909c1ae866637d2e489dcad3
ForkΨKILLET <fork_killet@qq.com>
2022-07-03 12:10:13 +0800

    fix(module/captcha): [migrate] works now

---
+++ src/modules/captcha/src/index.ts
@@ -0,0 +1,37 @@
+import '@exlg/core/types/module-entry'
+
+utils.mustMatch([/^\/discuss\/.*/, /^\/image/])
+
+let img: JQuery<HTMLImageElement> = $('img[data-v-3e1b4641], #verify_img')
+
+const autofill = async () => {
+    if (!img.length) return
+
+    const canvas = document.createElement('canvas')
+    canvas.width = img.width()!
+    canvas.height = img.height()!
+    canvas.getContext('2d')?.drawImage(img[0], 0, 0)
+
+    const input = $("input[placeholder$='验证码']")
+    input.val(
+        await utils.csPost(
+            'https://luogu-captcha-bypass.piterator.com/predict/',
+            canvas.toDataURL('image/jpeg')
+        ).responseText
+    )
+    input.trigger('input')
+}
+
+if (img) {
+    // Note: 登陆和讨论
+    img.trigger('click')
+    img.on('load', autofill)
+} else {
+    // Note: 图床
+    $(document).on('focus', "input[placeholder$='验证码']", () => {
+        img = $<HTMLImageElement>('#--swal-image-hosting-upload-captcha').on(
+            'load',
+            autofill
+        )
+    })
+}
