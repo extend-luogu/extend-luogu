2ced7e4b075fccdf674c04651f367715c751c19b
YuzhenQin <1@jin-dan.site>
2022-07-25 09:23:04 +0800

    ci(cypress): update mock & `pnpm upgrade -r`

--- cypress/e2e/mock.js
+++ cypress/e2e/mock.js
@@ -1,17 +1,6 @@
 /* eslint-disable no-eval */
 /* eslint-disable no-unused-vars */
 
-const utils = {
-    mustMatch: (_urls) => {},
-    csGet: (url) => {
-        return fetch(
-            url
-                .replace('xn--fx-ex2c330n.ml', 'www.luogu.com.cn')
-                .replace('fx白丝.ml', 'www.luogu.com.cn')
-        ).then((resp) => resp.json())
-    }
-}
-
 const runtime = {
     storage: {
         get: (x) => eval(x),
@@ -30,3 +19,48 @@ const Schema = {
 }
 
 const { log } = console
+
+const hookList = []
+const hooker = new MutationObserver((records) => {
+    const tmpNodeList = []
+    records.forEach((record) => {
+        record.addedNodes.forEach((addedNode) => tmpNodeList.push(addedNode))
+    })
+    hookList.forEach((hook) => hook(tmpNodeList))
+})
+
+const addHook = (hook) => {
+    hookList.push(hook)
+}
+
+const addHookAndCallback = (hook, callback) => {
+    addHook((insertedNodes) => {
+        const hookedNodes = hook(insertedNodes)
+        if (hookedNodes.length) callback(hookedNodes)
+    })
+}
+
+const addHookSelector = (selector, callback) => {
+    addHookAndCallback((insertedNodes) => {
+        const hookedNodes = []
+        insertedNodes.forEach((node) => {
+            if ($(node).is(selector)) hookedNodes.push(node)
+            hookedNodes.push(...$(node).find(selector).get())
+        })
+        return hookedNodes
+    }, callback)
+}
+
+const utils = {
+    mustMatch: (_urls) => {},
+    csGet: (url) => {
+        return fetch(
+            url
+                .replace('xn--fx-ex2c330n.ml', 'www.luogu.com.cn')
+                .replace('fx白丝.ml', 'www.luogu.com.cn')
+        ).then((resp) => resp.json())
+    },
+    addHookSelector
+}
+
+hooker.observe(document.body, { childList: true, subtree: true })
