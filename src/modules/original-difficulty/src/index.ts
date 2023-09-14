a3e27da074a9a375d03285e9dad20a6542ff33ca
5ab-juruo <xiaowucc2017@163.com>
2023-09-14 18:42:45 +0800

    feat(original-difficulty): improve CF api

--- src/modules/original-difficulty/src/index.ts
+++ src/modules/original-difficulty/src/index.ts
@@ -33,53 +33,27 @@ function makeDifficulty(rawDiff: number) {
     return Math.round(400 / Math.exp(1.0 - rawDiff / 400))
 }
 
-const fetchProcess = (async () => {
-    await utils.loadChore(
-        sto.get('_CFLastFetched'),
-        '10D',
-        (now) => sto.set('_CFLastFetched', now),
-        async () => {
-            const resp = (
-                await utils.csGet(
-                    'https://codeforces.com/api/problemset.problems?lang=en',
-                )
-            ).json as CFResponse<{
-                problems: CFProblem[]
-                problemStatistics: unknown[]
-            }>
-            if (resp.status !== 'OK') {
-                warn('Codeforces API unavailable.')
-                return
-            }
-            resp.result.problems.forEach((problem: CFProblem) => {
-                if (problem.rating) difficulty['CF' + problem.contestId + problem.index] = problem.rating
-            })
-            sto.set('_difficulty', difficulty)
-        },
-    )
+const fetchATDifficulty = utils.loadChore(
+    sto.get('_ATLastFetched'),
+    '1D',
+    (now) => sto.set('_ATLastFetched', now),
+    async () => {
+        const resp = (
+            await utils.csGet(
+                'https://kenkoooo.com/atcoder/resources/problem-models.json',
+            )
+        ).json as Record<string, ATProblemStatistics>
+        for (const key in resp) {
+            const tmpDiff = resp[key].difficulty
+            if (tmpDiff) difficulty[key] = makeDifficulty(tmpDiff)
+        }
 
-    await utils.loadChore(
-        sto.get('_ATLastFetched'),
-        '10D',
-        (now) => sto.set('_ATLastFetched', now),
-        async () => {
-            const resp = (
-                await utils.csGet(
-                    'https://kenkoooo.com/atcoder/resources/problem-models.json',
-                )
-            ).json as Record<string, ATProblemStatistics>
-            for (const key in resp) {
-                const tmpDiff = resp[key].difficulty
-                if (tmpDiff) difficulty[key] = makeDifficulty(tmpDiff)
-            }
-
-            sto.set('_difficulty', difficulty)
-        },
-    )
-})()
+        sto.set('_difficulty', difficulty)
+    },
+)
 
 let lastHookedUrl = ''
-utils.addHookSelector('div.stat > div.field', ({ hookedNodes }) => {
+utils.addHookSelector('div.stat > div.field', async ({ hookedNodes }) => {
     if (window.location.href === lastHookedUrl) return
     if (!/CF|AT/.test(window.location.pathname)) return
     lastHookedUrl = window.location.href
@@ -94,13 +68,36 @@ utils.addHookSelector('div.stat > div.field', ({ hookedNodes }) => {
     $title.text('原始难度')
     $status.text('获取中')
 
-    fetchProcess.then(() => $status.text(
-        difficulty[
-            window.location.pathname.includes('CF')
-                ? _feInstance.currentData.problem.pid
-                : _feInstance.currentData.problem.description
-                    .match(/^.{22}[-./A-Za-z0-9_]*/)[0]
-                    .match(/[^/]*$/)[0]
-        ] ?? '不可用',
-    ))
+    let currentDifficulty
+    if (window.location.pathname.includes('CF')) {
+        // better api: https://codeforces.com/apiHelp/methods#contest.standings
+        const problemId = _feInstance.currentData.problem.pid.substring(2)
+        const contestId = +problemId.match(/\d+/g)[0]
+        const inContestIndex = problemId.match(/[A-Z]\d*$/g)[0]
+        const resp = (
+            await utils.csGet(
+                `https://codeforces.com/api/contest.standings?contestId=${contestId}&from=1&count=1`,
+            )
+        ).json as CFResponse<{
+            problems: CFProblem[]
+        }>
+        if (resp.status !== 'OK') {
+            warn('Codeforces API unavailable.')
+        }
+        else {
+            for (const problem of resp.result.problems) {
+                if (problem.index === inContestIndex) {
+                    currentDifficulty = problem.rating
+                }
+            }
+        }
+    }
+    else {
+        await fetchATDifficulty
+        currentDifficulty = difficulty[_feInstance.currentData.problem.description
+            .match(/^.{22}[-./A-Za-z0-9_]*/)[0]
+            .match(/[^/]*$/)[0]
+        ]
+    }
+    $status.text(currentDifficulty ?? '不可用')
 })
--- src/modules/original-difficulty/src/schema.ts
+++ src/modules/original-difficulty/src/schema.ts
@@ -1,6 +1,4 @@
 export default Schema.object({
     _difficulty: Schema.dict(Schema.number()),
-    // WIP: _tags, _submissions
-    _CFLastFetched: Schema.number().default(-1),
     _ATLastFetched: Schema.number().default(-1),
 })
