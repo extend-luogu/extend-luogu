cc09ec8853c770d0517d0f8e70c891c30632377a
optimize-2 <55306215+optimize-2@users.noreply.github.com>
2023-09-14 16:34:18 +0800

    fix(module/captcha): adapt to the new frontend

--- src/modules/captcha/src/index.ts
+++ src/modules/captcha/src/index.ts
@@ -2,9 +2,9 @@ import '@exlg/core/types/module-entry'
 
 utils.mustMatch([/^\/discuss\/.*/, /^\/image/])
 
-let img: JQuery<HTMLImageElement> = $('img[data-v-3e1b4641], #verify_img')
-
-const autofill = async () => {
+const autofill = async (e: JQuery.TriggeredEvent) => {
+    const img = $(e.target)
+    console.log(e.target)
     if (!img.length) return
 
     const canvas = document.createElement('canvas')
@@ -12,7 +12,7 @@ const autofill = async () => {
     canvas.height = img.height()!
     canvas.getContext('2d')?.drawImage(img[0], 0, 0)
 
-    const input = $("input[placeholder$='验证码']")
+    const input = $("input[placeholder$='右侧图形验证码']")
 
     const res = (
         await utils.csPost(
@@ -25,17 +25,19 @@ const autofill = async () => {
     input.trigger('input')
 }
 
-if (img) {
-    // Note: 登陆和讨论
-    img.trigger('click')
-    img.on('load', autofill)
-}
-else {
-    // Note: 图床
-    $(document).on('focus', "input[placeholder$='验证码']", () => {
-        img = $<HTMLImageElement>('#--swal-image-hosting-upload-captcha').on(
-            'load',
-            autofill,
-        )
+utils.addHookAndCallback((insertedNodes) => {
+    let hookedNodes: Node[] = []
+    insertedNodes.forEach((e) => {
+        const $div = $(e).find('div[placeholder=右侧图形验证码]')
+        const img = $div[0]?.nextElementSibling?.firstChild
+        if (img) hookedNodes.push(img)
     })
-}
+    hookedNodes = Array.from(new Set(hookedNodes))
+    return { hookedNodes }
+}, async ({ hookedNodes, info }) => {
+    hookedNodes.forEach((e) => {
+        const $img = $(e as HTMLImageElement)
+        $img.trigger('click')
+        $img.on('load', autofill)
+    })
+})
