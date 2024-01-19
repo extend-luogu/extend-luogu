a666027db317b016a991cc1bf571597e23173d73
minstdfx <3538742776@qq.com>
2024-01-20 00:07:54 +0800

    feat(module/show-user-desc): v1.0.0

---
+++ src/modules/show-user-desc/src/index.ts
@@ -0,0 +1,19 @@
+import '@exlg/core/types/module-entry'
+
+utils.mustMatch([/^\/user\/\d+$/])
+
+const func = (args: JQuery<HTMLElement>) => {
+    const isMainPage: boolean = ['', 'main'].includes(window.location.hash)
+    if (isMainPage) {
+        args.prev().remove()
+        args.show()
+    }
+}
+
+utils.addHookSelector('section.main .card.padding-default .introduction.marked', ({ hookedNodes }) => {
+    hookedNodes.forEach((node) => {
+        if (node.nodeType === node.ELEMENT_NODE) {
+            func($(node as HTMLElement))
+        }
+    })
+})
